30b91ce23b7ee2a3b8b496dcef2133c494274c0b
Drew Hoek <drewh815@gmail.com>
2023-03-15 11:00:31 -0500

    User rotuter updated to expect different parameters, working correctly

--- database.sql
+++ database.sql
@@ -1,10 +1,65 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" serial PRIMARY KEY NOT NULL,
+	"first_name" varchar(20) NOT NULL,
+	"last_name" varchar(25) NOT NULL,
+	"email_address" TEXT NOT NULL,
+	"password" varchar(25) NOT NULL,
+	"phone_number" int NOT NULL,
+	"is_verified" BOOLEAN NOT NULL DEFAULT 'FALSE',
+	"is_admin" BOOLEAN NOT NULL DEFAULT 'FALSE'
+);
+
+
+
+CREATE TABLE "tags" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"tag_name" varchar(25) NOT NULL
+);
+
+
+
+CREATE TABLE "locations" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"location_name" varchar(75) NOT NULL
+);
+
+
+CREATE TABLE "tasks" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" TEXT NOT NULL,
+	"notes" TEXT,
+	"has_budget" BOOLEAN NOT NULL DEFAULT 'FALSE',
+	"budget" FLOAT,
+	"location_id" int NOT NULL REFERENCES "locations"("id"),
+	"status" TEXT NOT NULL,
+	"created_by_id" int NOT NULL REFERENCES "user"("id"),
+	"assigned_to_id" int NOT NULL REFERENCES "user"("id"),
+	"time_created" TIMESTAMP NOT NULL,
+	"time_assigned" TIMESTAMP,
+	"time_completed" TIMESTAMP,
+	"is_time_sensitive" BOOLEAN NOT NULL DEFAULT 'FALSE',
+	"due_date" DATE,
+	"is_approved" BOOLEAN NOT NULL DEFAULT 'FALSE'
+);
+
+
+CREATE TABLE "tags_per_task" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"task_id" int NOT NULL REFERENCES "tasks"("id"),
+	"tag_id" int NOT NULL REFERENCES "tags"("id")
+);
+
+CREATE TABLE "comments" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"task_id" serial NOT NULL REFERENCES "tasks"("id"),
+	"time_posted" TIMESTAMP NOT NULL,
+	"content" TEXT NOT NULL,
+	"posted_by_id" int NOT NULL REFERENCES "user"("id")
+);
+
+CREATE TABLE "photos" (
+    "id" serial PRIMARY KEY NOT NULL,
+    "task_id" serial NOT NULL REFERENCES "tasks"("id"),
+    "photo_link"
+
 );
--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -18,13 +18,15 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 // The only thing different from this and every other post we've seen
 // is that the password gets encrypted before being inserted
 router.post('/register', (req, res, next) => {
-  const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
-
-  const queryText = `INSERT INTO "user" (username, password)
-    VALUES ($1, $2) RETURNING id`;
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+  const phone_number = req.body.phone_number;
+  const email_address = req.body.email_address;
+  const queryText = `INSERT INTO "user" ("first_name", "last_name", "email_address", "password", "phone_number")
+  VALUES ($1, $2, $3, $4, $5) RETURNING id;`;
   pool
-    .query(queryText, [username, password])
+    .query(queryText, [first_name, last_name, email_address, password, phone_number])
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log('User registration failed: ', err);
