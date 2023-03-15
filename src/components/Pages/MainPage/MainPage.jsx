0f6e8fc8b84e50f88a160e4d2c242356359afda2
Drew Hoek <drewh815@gmail.com>
2023-03-15 15:22:19 -0500

    Tab functionality started, saga started for getting tasks

--- src/components/Pages/MainPage/MainPage.jsx
+++ src/components/Pages/MainPage/MainPage.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import LogOutButton from "../../Shared/LogOutButton/LogOutButton";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
+import { AppBar, Tab, Tabs, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 function UserPage() {
 	const dispatch = useDispatch();
 	const user = useSelector((store) => store.user);
 	const unverifiedUsers = useSelector((store) => store.unverifiedUsersReducer);
+
+	const [tabIndex, setTabIndex] = useState(0);
+
+	const handleTabChange = (event, newTabIndex) => {
+		setTabIndex(newTabIndex);
+	};
 
 	useEffect(() => {
 		dispatch({ type: "FETCH_UNVERIFIED_USERS" });
@@ -13,10 +21,25 @@ function UserPage() {
 
 	return (
 		<div className="container">
-			<h2>Welcome, {user.first_name}!</h2>
-			<p>Your ID is: {user.id}</p>
-			<pre>{JSON.stringify(user)}</pre>
-			<pre>{JSON.stringify(unverifiedUsers)}</pre>
+			<Box>
+				<Tabs value={tabIndex} onChange={handleTabChange}>
+					<Tab label="Dashboard" />
+					<Tab label="Manage Users" />
+					<Tab label="Manage Tasks" />
+				</Tabs>
+			</Box>
+			<Box>
+				{tabIndex === 0 && (
+					<Box>
+						<h2>Welcome, {user.first_name}!</h2>
+						<p>Your ID is: {user.id}</p>
+						<pre>{JSON.stringify(user)}</pre>
+						<pre>{JSON.stringify(unverifiedUsers)}</pre>
+					</Box>
+				)}
+				{tabIndex === 1 && <Box></Box>}
+				{tabIndex === 2 && <Box></Box>}
+			</Box>
 
 			<LogOutButton className="btn" />
 		</div>
