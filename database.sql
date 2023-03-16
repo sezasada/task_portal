4735307a778f4f1337e325b308ef8648c9b39dd9
Drew Hoek <drewh815@gmail.com>
2023-03-16 16:21:40 -0500

    modified database.sql file again and added BIG query

--- database.sql
+++ database.sql
@@ -60,3 +60,47 @@ VALUES ('maintenance'), ('cleaning'), ('farm work'), ('administrative');
 
 INSERT INTO "locations" ("location_name")
 VALUES ('north'), ('east'), ('south'), ('west');
+
+-- HERE IS THE BIGGEST JOIN IN THE WORLD -- 
+SELECT "tasks"."id" AS "task_id", "title", "notes", "has_budget", "budget", "location_id", "status", 
+    created_by."id" AS "created_by_id", 
+    created_by."first_name" AS "created_by_first_name", 
+    created_by."last_name" AS "created_by_last_name", 
+    created_by."username" AS "created_by_username", 
+    created_by."phone_number" AS "created_by_phone_number", 
+    assigned_to."id" AS "assigned_to_id", 
+    assigned_to."first_name" AS "assigned_to_first_name", 
+    assigned_to."last_name" AS "assigned_to_last_name", 
+    assigned_to."username" AS "assigned_to_username", 
+    assigned_to."phone_number" AS "assigned_to_phone_number", 
+    "time_created", "time_assigned", "time_completed", "is_time_sensitive", "due_date", "location_name", 
+    json_agg(
+        json_build_object(
+            'tag_id', "tags"."id",
+            'tag_name', "tags"."tag_name"
+        )
+    ) AS "tags",
+    json_agg(
+        json_build_object(
+            'comment_id', "comments"."id",
+            'time_posted', "comments"."time_posted",
+            'content', "comments"."content",
+            'posted_by_first_name', posted_by."first_name",
+            'posted_by_last_name', posted_by."last_name",
+            'posted_by_username', posted_by."username",
+            'posted_by_phone_number', posted_by."phone_number"
+        )
+    ) AS "comments"
+FROM "tasks"
+JOIN "locations" ON "location_id" = "locations"."id"
+JOIN "user" created_by ON created_by."id" = "tasks"."created_by_id"
+JOIN "user" assigned_to ON assigned_to."id" = "tasks"."assigned_to_id"
+JOIN "tags_per_task" ON "task_id" = "tasks"."id"
+JOIN "tags" ON "tag_id" = "tags"."id"
+JOIN "comments" ON "tasks"."id" = "comments"."task_id"
+JOIN "user" posted_by ON posted_by."id" = "comments"."posted_by_id"
+WHERE "is_approved" = TRUE
+GROUP BY "tasks"."id", "title", "notes", "has_budget", "budget", "location_id", "status", 
+    created_by."id", created_by."first_name", created_by."last_name", created_by."username", created_by."phone_number", 
+    assigned_to."id", assigned_to."first_name", assigned_to."last_name", assigned_to."username", assigned_to."phone_number", 
+    "time_created", "time_assigned", "time_completed", "is_time_sensitive", "due_date", "location_name";
