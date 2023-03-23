8a6cd47b95d88cbccb4c35e0b7fb2f41b0500914
Sebastien Zasada <sezasada@icloud.com>
2023-03-23 09:24:12 -0500

    set up the user page components. conditionally rendered the tabs to display based on if a user is_admin === true or false

--- src/components/Pages/MainPage/MainPage.jsx
+++ src/components/Pages/MainPage/MainPage.jsx
@@ -4,13 +4,21 @@ import { Box } from "@mui/system";
 import AdminDashboard from "./TabComponents/Admin/AdminDashboard";
 import AdminManageUsers from "./TabComponents/Admin/AdminManageUsers";
 import AdminManageTasks from "./TabComponents/Admin/AdminManageTasks";
-
+import UserDashboard from "./TabComponents/User/UserDashboard";
+import UserCreateTask from "./TabComponents/User/UserCreateTask";
+import UserTaskList from "./TabComponents/User/UserTaskList";
 function UserPage() {
 	const dispatch = useDispatch();
 
 	// Access tab index reducer
 	const tabIndex = useSelector((store) => store.tabIndexReducer);
-
+	// Access unverified user reducer
+	const unverifiedUsers = useSelector((store) => store.unverifiedUsersReducer);
+	// Access verified user reducer
+	const verifiedUsers = useSelector((store) => store.verifiedUsersReducer);
+	const infoOfSpecificUser = useSelector(
+		(store) => store.viewAccountInfoReducer
+	);
 	// Grab all needed data on page load
 	useEffect(() => {
 		dispatch({ type: "FETCH_UNVERIFIED_USERS" });
@@ -26,12 +34,23 @@ function UserPage() {
 		dispatch({ type: "FETCH_ALL_COMPLETED_TASKS" });
 	}, []);
 
+
 	return (
 		<div className="container">
 			<Box>
-				{tabIndex === 0 && <AdminDashboard />}
-				{tabIndex === 1 && <AdminManageUsers />}
-				{tabIndex === 2 && <AdminManageTasks />}
+				{infoOfSpecificUser.is_admin ? (
+					<>
+						{tabIndex === 0 && <AdminDashboard />}
+						{tabIndex === 1 && <AdminManageUsers />}
+						{tabIndex === 2 && <AdminManageTasks />}
+					</>
+				) : (
+					<>
+						{tabIndex === 0 && <UserDashboard />}
+						{tabIndex === 1 && <UserCreateTask />}
+						{tabIndex === 2 && <UserTaskList />}
+					</>
+				)}
 			</Box>
 		</div>
 	);
