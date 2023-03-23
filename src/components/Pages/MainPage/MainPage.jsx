4be1914141a1adbcfd6f9bea5ae97d64f2162f1f
Drew Hoek <drewh815@gmail.com>
2023-03-23 10:54:16 -0500

    fixed conditional render for user and admin pages

--- src/components/Pages/MainPage/MainPage.jsx
+++ src/components/Pages/MainPage/MainPage.jsx
@@ -12,13 +12,8 @@ function UserPage() {
 
 	// Access tab index reducer
 	const tabIndex = useSelector((store) => store.tabIndexReducer);
-	// Access unverified user reducer
-	const unverifiedUsers = useSelector((store) => store.unverifiedUsersReducer);
-	// Access verified user reducer
-	const verifiedUsers = useSelector((store) => store.verifiedUsersReducer);
-	const infoOfSpecificUser = useSelector(
-		(store) => store.viewAccountInfoReducer
-	);
+
+	const user = useSelector((store) => store.user);
 	// Grab all needed data on page load
 	useEffect(() => {
 		dispatch({ type: "FETCH_UNVERIFIED_USERS" });
@@ -32,11 +27,10 @@ function UserPage() {
 		dispatch({ type: "FETCH_COMPLETED_USER_TASKS" });
 	}, []);
 
-
 	return (
 		<div className="container">
 			<Box>
-				{infoOfSpecificUser.is_admin ? (
+				{user.is_admin ? (
 					<>
 						{tabIndex === 0 && <AdminDashboard />}
 						{tabIndex === 1 && <AdminManageUsers />}
