132b6f5497c29f116ba9c264c041fa4f7469fefe
Sebastien Zasada <sezasada@icloud.com>
2023-03-24 14:00:42 -0500

    modified the profile page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import {
-	HashRouter as Router,
-	Redirect,
-	Route,
-	Switch,
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
 } from "react-router-dom";
 
 import { useDispatch, useSelector } from "react-redux";
@@ -26,100 +26,94 @@ import Cloudinary from "./Pages/Cloudinary";
 import "./App.css";
 
 function App() {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const user = useSelector((store) => store.user);
+  const user = useSelector((store) => store.user);
 
-	useEffect(() => {
-		dispatch({ type: "FETCH_USER" });
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch({ type: "FETCH_USER" });
+  }, [dispatch]);
 
-	return (
-		<Router>
-			<div>
-				<Nav />
-				<Switch>
-					{/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-					<Redirect exact from="/" to="/home" />
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+          <Redirect exact from="/" to="/home" />
 
-					{/* Visiting localhost:3000/about will show the about page. */}
-					<Route
-						// shows AboutPage at all times (logged in or not)
-						exact
-						path="/about"
-					>
-						<Profile />
-					</Route>
+          {/* Visiting localhost:3000/about will show the about page. */}
 
-					{/* For protected routes, the view could show one of several things on the same route.
+          {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-					<ProtectedRoute
-						// logged in shows UserPage else shows LoginPage
-						exact
-						path="/main"
-					>
-						<MainPage />
-					</ProtectedRoute>
+          <ProtectedRoute
+            // logged in shows UserPage else shows LoginPage
+            exact
+            path="/main"
+          >
+            <MainPage />
+          </ProtectedRoute>
+		  <ProtectedRoute exact path="/about">
+            <Profile />
+          </ProtectedRoute>
 
-					<Route exact path="/login">
-						{user.id ? (
-							// If the user is already logged in,
-							// redirect to the /user page
-							<Redirect to="/main" />
-						) : (
-							// Otherwise, show the login page
-							<LoginPage />
-						)}
-					</Route>
+          <Route exact path="/login">
+            {user.id ? (
+              // If the user is already logged in,
+              // redirect to the /user page
+              <Redirect to="/main" />
+            ) : (
+              // Otherwise, show the login page
+              <LoginPage />
+            )}
+          </Route>
 
-					<Route exact path="/registration">
-						{user.id ? (
-							// If the user is already logged in,
-							// redirect them to the /user page
-							<Redirect to="/main" />
-						) : (
-							// Otherwise, show the registration page
-							<RegisterPage />
-						)}
-					</Route>
+          <Route exact path="/registration">
+            {user.id ? (
+              // If the user is already logged in,
+              // redirect them to the /user page
+              <Redirect to="/main" />
+            ) : (
+              // Otherwise, show the registration page
+              <RegisterPage />
+            )}
+          </Route>
 
-					<Route exact path="/home">
-						{user.id ? (
-							// If the user is already logged in,
-							// redirect them to the /user page
-							<Redirect to="/main" />
-						) : (
-							// Otherwise, show the Landing page
-							<LandingPage />
-						)}
-					</Route>
-					<Route path="/reset">
-					<ResetPassword />
-				</Route>
-				<Route path="/request_reset">
-					<RequestReset />
-				</Route>
-				<Route path="/email_sent">
-					<EmailSent />
-				</Route>
-				<Route path="/cloudinary">
-					<Cloudinary />
-				</Route>
+          <Route exact path="/home">
+            {user.id ? (
+              // If the user is already logged in,
+              // redirect them to the /user page
+              <Redirect to="/main" />
+            ) : (
+              // Otherwise, show the Landing page
+              <LandingPage />
+            )}
+          </Route>
+          <Route path="/reset">
+            <ResetPassword />
+          </Route>
+          <Route path="/request_reset">
+            <RequestReset />
+          </Route>
+          <Route path="/email_sent">
+            <EmailSent />
+          </Route>
+          <Route path="/cloudinary">
+            <Cloudinary />
+          </Route>
 
-					{/* If none of the other routes matched, we will show a 404. */}
-					<Route>
-						<h1>404</h1>
-					</Route>
-				</Switch>
+          {/* If none of the other routes matched, we will show a 404. */}
+          <Route>
+            <h1>404</h1>
+          </Route>
+        </Switch>
 
-				
-				<Footer />
-				
-			</div>
-		</Router>
-	);
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
