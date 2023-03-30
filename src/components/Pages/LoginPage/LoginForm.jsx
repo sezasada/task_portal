e3d3a041acfbbc18ef144b01b3a59694bc8ff717
Kathryn Szombatfalvy <kathrynszombatfalvy@gmail.com>
2023-03-30 10:10:01 -0500

    styled the landing, login, and register page

--- src/components/Pages/LoginPage/LoginForm.jsx
+++ src/components/Pages/LoginPage/LoginForm.jsx
@@ -3,100 +3,139 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { Stack } from "@mui/system";
 
 function LoginForm() {
-	const history = useHistory();
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const errors = useSelector((store) => store.errors);
-	const dispatch = useDispatch();
+  const history = useHistory();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const errors = useSelector((store) => store.errors);
+  const dispatch = useDispatch();
 
-	const login = (event) => {
-		event.preventDefault();
+  const login = (event) => {
+    event.preventDefault();
 
-		if (username && password) {
-			dispatch({
-				type: "LOGIN",
-				payload: {
-					username: username,
-					password: password,
+    if (username && password) {
+      dispatch({
+        type: "LOGIN",
+        payload: {
+          username: username,
+          password: password,
+        },
+      });
+    } else {
+      dispatch({ type: "LOGIN_INPUT_ERROR" });
+    }
+  }; // end login
+
+  return (
+    <Stack
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "8px",
+        marginBottom: "10px",
+      }}
+      spacing={3}
+    >
+      <Paper
+        sx={{ p: 3, backgroundColor: "rgb(241, 241, 241)", maxWidth: "750px" }}
+        elevation={3}
+      >
+        <form onSubmit={login}>
+		<Stack
+		sx={{
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+			
+		  }}>
+          <Typography
+            component="h2"
+            variant="h4"
+            sx={{ 
+				textDecoration: "underline",
+				color: "rgb(187, 41, 46)" }}
+          >
+            Login
+          </Typography>
+          {errors.loginMessage && (
+            <Typography
+              variant="h6"
+              component="h3"
+              className="alert"
+              role="alert"
+            >
+              {errors.loginMessage}
+            </Typography>
+          )}
+          <br />
+          <Box>
+            <TextField
+              type="email"
+              name="username"
+              label="Email"
+              required
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </Box>
+          <br />
+          <Box>
+            <TextField
+              type="password"
+              name="password"
+              label="Password"
+              required
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </Box>
+          <br />
+          <Box>
+            <Button
+              variant="contained"
+              type="submit"
+              name="submit"
+              value="Log In"
+			  sx={{
+				color: "white",
+				backgroundColor: "rgb(187, 41, 46)",
+				"&:hover": {
+				  backgroundColor: "rgb(187, 41, 46)",
+				  transform: "scale(1.03)",
 				},
-			});
-		} else {
-			dispatch({ type: "LOGIN_INPUT_ERROR" });
-		}
-	}; // end login
-
-	return (
-		<Paper
-			sx={{ p: 3, backgroundColor: "rgb(241, 241, 241)", maxWidth: "750px" }}
-			elevation={3}
-		>
-			<form onSubmit={login}>
-				<Typography
-					component="h2"
-					variant="h4"
-					sx={{ textDecoration: "underline" }}
-				>
-					Login
-				</Typography>
-				{errors.loginMessage && (
-					<Typography
-						variant="h6"
-						component="h3"
-						className="alert"
-						role="alert"
-					>
-						{errors.loginMessage}
-					</Typography>
-				)}
-				<br />
-				<Box>
-					<TextField
-						type="email"
-						name="username"
-						label="Email"
-						required
-						value={username}
-						onChange={(event) => setUsername(event.target.value)}
-					/>
-				</Box>
-				<br />
-				<Box>
-					<TextField
-						type="password"
-						name="password"
-						label="Password"
-						required
-						value={password}
-						onChange={(event) => setPassword(event.target.value)}
-					/>
-				</Box>
-				<br />
-				<Box>
-					<Button
-						variant="contained"
-						type="submit"
-						name="submit"
-						value="Log In"
-					>
-						Log In
-					</Button>
-				</Box>
-				<br />
-				<Button
-					variant="contained"
-					type="button"
-					className="btn btn_sizeSm"
-					onClick={() => {
-						history.push("/request_reset");
-					}}
-				>
-					Reset Password
-				</Button>
-			</form>
-		</Paper>
-	);
+				width: 200,
+			}}
+            >
+              Log In
+            </Button>
+          </Box>
+          <br />
+          <Button
+            variant="contained"
+            type="button"
+            className="btn btn_sizeSm"
+            onClick={() => {
+              history.push("/request_reset");
+            }}
+			sx={{
+				color: "white",
+				backgroundColor: "rgb(187, 41, 46)",
+				"&:hover": {
+				  backgroundColor: "rgb(187, 41, 46)",
+				  transform: "scale(1.03)",
+				},
+				width: 200,
+			}}>
+            Reset Password
+          </Button>
+		  </Stack>
+        </form>
+      </Paper>
+    </Stack>
+  );
 }
 
 export default LoginForm;
--- src/components/Pages/LoginPage/LoginPage.jsx
+++ src/components/Pages/LoginPage/LoginPage.jsx
@@ -2,34 +2,67 @@ import React from "react";
 import LoginForm from "./LoginForm";
 import { useHistory } from "react-router-dom";
 import { Box } from "@mui/material";
+import { Paper, Button } from "@mui/material";
+import { Stack } from "@mui/system";
 
 function LoginPage() {
-	const history = useHistory();
+  const history = useHistory();
 
-	return (
-		<Box
-			sx={{
-				display: "flex",
-				flexDirection: "column",
-				alignItems: "center",
-				p: 3,
-			}}
-		>
-			<LoginForm />
-			<br />
-			<center>
-				<button
-					type="button"
-					className="btn btn_asLink"
-					onClick={() => {
-						history.push("/registration");
-					}}
-				>
-					Register
-				</button>
-			</center>
-		</Box>
-	);
+  return (
+    <Stack
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "4vh",
+      }}
+      spacing={3}
+    >
+      <Paper
+        sx={{
+          p: 3,
+          maxWidth: "35vh",
+          width: "90%",
+          backgroundColor: "rgb(241, 241, 241)",
+        }}
+        elevation={3}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            p: 3,
+          }}
+        >
+          <LoginForm />
+          <br />
+          <center>
+            <Button
+              type="button"
+              varient="contained"
+            className="btn btn_asLink"
+              onClick={() => {
+                history.push("/registration");
+              }}
+			  sx={{
+				color: "white",
+				backgroundColor: "rgb(187, 41, 46)",
+				"&:hover": {
+				  backgroundColor: "rgb(187, 41, 46)",
+				  transform: "scale(1.03)",
+				  color: "white",
+				},
+				width: 200,
+			  }}
+            >
+              Register
+            </Button>
+          </center>
+        </Box>
+      </Paper>
+    </Stack>
+  );
 }
 
 export default LoginPage;
