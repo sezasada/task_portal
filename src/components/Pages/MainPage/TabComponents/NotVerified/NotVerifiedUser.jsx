990b4e766e7372b00e6bf631ff004a85b43f875e
Sebastien Zasada <sezasada@icloud.com>
2023-03-30 15:50:33 -0500

    done with the profile page

--- src/components/Pages/MainPage/TabComponents/NotVerified/NotVerifiedUser.jsx
+++ src/components/Pages/MainPage/TabComponents/NotVerified/NotVerifiedUser.jsx
@@ -1,10 +1,39 @@
-function NotVerifiedUser () {
+import { Typography, Box, Paper } from "@mui/material";
+import { Stack } from "@mui/system";
 
-    return (
-        <div>
-            <h1>GET VERIFIED</h1>
-        </div>
-    );
+import "./NotVerifiedUser.css";
+function NotVerifiedUser() {
+  return (
+    <Stack
+      spacing={3}
+      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+    >
+      <Paper
+        sx={{
+          p: 3,
+          maxWidth: "750px",
+          marginTop: "10%",
+          width: "90%",
+          backgroundColor: "rgb(241, 241, 241)",
+        }}
+        elevation={3}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20%",
+            marginBottom: "20%",
+          }}
+        >
+          <Typography>
+            <span className="text-animation">Account successfully created</span>
+            <span className="text-animation">Waiting to be verified</span>
+          </Typography>
+        </Box>
+      </Paper>
+    </Stack>
+  );
 }
 
 export default NotVerifiedUser;
